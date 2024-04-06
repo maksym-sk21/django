9ad50be68e4b2d0eef98359264c295ce51cd2465
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-04-06 19:21:35 +0300

    Add files via upload

---
+++ quotes_project/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth import logout
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
